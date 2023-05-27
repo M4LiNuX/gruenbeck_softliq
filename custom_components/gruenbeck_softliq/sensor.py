e440c1d3945378d931f04b51c2012d9ef37269d4
Manuel Janocha <manuel.janocha@gmx.de>
2023-05-27 22:12:29 +0200

    add example sensor

--- custom_components/gruenbeck_softliq/sensor.py
+++ custom_components/gruenbeck_softliq/sensor.py
@@ -1,31 +1,38 @@
-# """SoftliQ Sensor Platform."""
-# import logging
-# import voluptuous as vol
+"""Platform for sensor integration."""
+from __future__ import annotations
 
-# from homeassistant.components.sensor import (
-#     PLATFORM_SCHEMA,
-#     SensorDeviceClass, 
-#     SensorEntity, 
-#     SensorStateClass
-# )
-# from homeassistant.cure import HomeAssistant
-# from homeassistant.helpers.entity_platform import AddEntitiesCallback
-# from homeassistant.helpers.typing import ConfigType, DisoveryInfoType
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import TEMP_CELSIUS
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-# from homeassistant.const import (
-#     CONF_NAME,
-#     CONF_ADDR
-# )
 
-# DEFAULT_NAME = "SoftliQ - Watersoftener"
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None
+) -> None:
+    """Set up the sensor platform."""
+    add_entities([ExampleSensor()])
 
-# PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-#     {
-#         vol.Required(CONF_NAME, default=DEFAULT_NAME): cv.string,
-#         vol.Required(CONF_ADDR): cv.string
-#     }
-# )
 
-# _LOGGER = logging.getLogger(__name__)
-# # Time between updating data from softliq
-# SCAN_INTERVAL = timedelta(seconds=10)
+class ExampleSensor(SensorEntity):
+    """Representation of a Sensor."""
+
+    _attr_name = "Example Temperature"
+    _attr_native_unit_of_measurement = TEMP_CELSIUS
+    _attr_device_class = SensorDeviceClass.TEMPERATURE
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._attr_native_value = 23
